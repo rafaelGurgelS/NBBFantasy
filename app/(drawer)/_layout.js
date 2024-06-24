d406a2a6d592d63d56262d8759c1c467e74ab75b
rafaelGurgelS <rafaelgs@gmail.com>
2024-06-24 01:45:17 -0300

    fixing design

--- app/(drawer)/_layout.js
+++ app/(drawer)/_layout.js
@@ -1,39 +1,73 @@
+import React, { useState, useEffect } from 'react';
 import { Drawer } from 'expo-router/drawer';
+import { useNavigationState } from '@react-navigation/native';
+
+const routeTitles = {
+  home: 'Home',
+  escalacao: 'Escalação',
+  ranking: 'Ranking',
+  partidas: 'Partidas',
+  premium: 'Premium',
+  conta: 'Minha Conta',
+  time: 'Meu Time',
+  '(tabs)': 'Home', // Define o título inicial para as tabs
+};
 
 export default function DrawerLayout() {
+  const [headerTitle, setHeaderTitle] = useState('Home'); // Define "Home" como título inicial
+  const state = useNavigationState((state) => state);
+
+  useEffect(() => {
+    const findActiveRoute = (state) => {
+      if (!state || !state.routes) return '(tabs)';
+      const route = state.routes[state.index];
+      if (route.state) {
+        return findActiveRoute(route.state);
+      }
+      return route.name;
+    };
+
+    const activeRoute = findActiveRoute(state);
+
+    // Verifique se o título atual é '(drawer)' e ajuste para o título inicial correto
+    if (activeRoute === '(drawer)' || activeRoute === '(tabs)') {
+      setHeaderTitle('Home');
+    } else {
+      setHeaderTitle(routeTitles[activeRoute] || activeRoute);
+    }
+  }, [state]);
+
   return (
     <Drawer
-    screenOptions={{ tabBarActiveTintColor: '#FC9904', 
-      headerTitleAlign: 'center',
-      headerStyle: {
-        borderBottomWidth: 2, // Espessura da linha
-        borderBottomColor: '#FC9904', // Cor laranja para a linha
-      },
-     }}>
-      {/* Defina rotas do Drawer com base na estrutura do sistema de arquivos */}
+      screenOptions={{
+        headerTitle: headerTitle,
+        headerTitleAlign: 'center',
+        headerStyle: {
+          borderBottomWidth: 2,
+          borderBottomColor: '#FC9904',
+        },
+      }}
+    >
       <Drawer.Screen
-        name="(tabs)" 
+        name="(tabs)"
         options={{
-          drawerItemStyle: { display: 'none' }, // Oculta esta tela do menu do Drawer
-          drawerLabel: 'tabs',
+          drawerItemStyle: { display: 'none' },
+          drawerLabel: 'Tabs',
           drawerActiveBackgroundColor: '#FC9904',
-          title: '', // O título pode ser atualizado dinamicamente
         }}
       />
       <Drawer.Screen
-        name="conta" // Rota para a tela "Minha Conta"
+        name="conta"
         options={{
           drawerLabel: 'Minha Conta',
           drawerActiveBackgroundColor: '#FC9904',
-          title: 'Minha Conta',
         }}
       />
       <Drawer.Screen
-        name="time" // Rota para a tela "Meu Time"
+        name="time"
         options={{
           drawerLabel: 'Meu Time',
           drawerActiveBackgroundColor: '#FC9904',
-          title: 'Meu Time',
         }}
       />
     </Drawer>
