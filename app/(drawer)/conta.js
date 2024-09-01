5a8b945a6732bb5e8e09b0e0d93603cde6106659
rafaelGurgelS <rafaelgs@gmail.com>
2024-09-01 00:05:37 -0300

    Screen design

--- app/(drawer)/conta.js
+++ app/(drawer)/conta.js
@@ -1,56 +1,230 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons'; // Biblioteca de ícones
+import React, { useState } from 'react';
+import {
+  VStack,
+  Heading,
+  Input,
+  Text,
+  Button,
+  Icon,
+  useToast,
+} from 'native-base';
+import { TouchableOpacity, ImageBackground, StyleSheet, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'; 
+import { useRouter } from 'expo-router';
 
 export default function Conta() {
-  const navigation = useNavigation();
+  const router = useRouter(); 
+  const toast = useToast();
+
+  const [email, setEmail] = useState('user@example.com');
+  const [senha, setSenha] = useState('********');
+  const [isEditingEmail, setIsEditingEmail] = useState(false);
+  const [isEditingSenha, setIsEditingSenha] = useState(false);
+
+  const toggleEditEmail = () => {
+    setIsEditingEmail(!isEditingEmail);
+    if (isEditingEmail) {
+      toast.show({
+        title: "Sucesso",
+        description: "Email editado com sucesso!",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
+
+  const toggleEditSenha = () => {
+    setIsEditingSenha(!isEditingSenha);
+    if (isEditingSenha) {
+      toast.show({
+        title: "Sucesso",
+        description: "Senha editada com sucesso!",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
+
+  const handleInputClick = (type) => {
+    if (type === 'email' && !isEditingEmail) {
+      toast.show({
+        title: "Atenção",
+        description: "Clique no ícone de lápis para editar o email.",
+        status: "warning",
+        duration: 3000,
+        isClosable: true,
+      });
+    } else if (type === 'senha' && !isEditingSenha) {
+      toast.show({
+        title: "Atenção",
+        description: "Clique no ícone de lápis para editar a senha.",
+        status: "warning",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
+
+  const handleDeleteAccount = () => {
+    toast.show({
+      title: "Confirmação",
+      description: "Tem certeza de que deseja excluir sua conta?",
+      status: "warning",
+      duration: 3000,
+      isClosable: true,
+    });
+  };
 
   return (
-    <View style={{ flex: 1 }}>
-      {/* Header */}
-      <View style={styles.header}>
-        <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
-          <Ionicons name="arrow-back" size={24} color="black" />
-        </TouchableOpacity>
-        <Text style={styles.headerTitle}>Minha Conta</Text>
-      </View>
-      <View style={styles.headerUnderline} />
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('../../assets/images/layoutDrawerConta.jpg')} 
+        style={styles.imageBackground}
+        resizeMode="cover"
+      >
+        <VStack
+          width="85%"
+          space={5}
+          alignItems="center"
+          padding={4}
+          borderRadius="lg"
+          style={styles.vStack}
+        >
+          <TouchableOpacity
+            style={styles.backButton}
+            onPress={() => router.push('/(drawer)/(tabs)/home')}
+          >
+            <Icon as={MaterialIcons} name="arrow-back" size={10} color="#FFFFFF" />
+          </TouchableOpacity>
 
-      {/* Main Content */}
-      <View style={styles.content}>
-        <Text style={styles.text}>Conteúdo da Minha Conta</Text>
-      </View>
+          <Heading size="lg" color="#FFFFFF" mb={4}>
+            SUA CONTA NBB FANTASY!
+          </Heading>
+
+          <View style={styles.textContainer}>
+            <Text fontSize={25} color="#FFFFFF" textAlign="left">
+              Email:
+            </Text>
+          </View>
+
+          <View style={styles.inputContainer}>
+            <Input
+              placeholder="Email"
+              variant="filled"
+              width="100%"
+              backgroundColor="#D9D9D9"
+              borderRadius="full"
+              height={50}
+              fontSize={16}
+              value={email}
+              onChangeText={setEmail}
+              isReadOnly={!isEditingEmail}
+              mb={4}
+              onTouchStart={() => handleInputClick('email')}
+            />
+            <TouchableOpacity
+              style={styles.editButton}
+              onPress={toggleEditEmail}
+            >
+              <Icon as={MaterialIcons} name={isEditingEmail ? "check" : "edit"} size={6} color="#FFFFFF" />
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.textContainer}>
+            <Text fontSize={25} color="#FFFFFF" textAlign="left">
+              Senha:
+            </Text>
+          </View>
+
+          <View style={styles.inputContainer}>
+            <Input
+              placeholder="Senha"
+              variant="filled"
+              width="100%"
+              backgroundColor="#D9D9D9"
+              borderRadius="full"
+              height={50}
+              fontSize={16}
+              value={senha}
+              onChangeText={setSenha}
+              isReadOnly={!isEditingSenha}
+              mb={6} // Espaço aumentado entre o campo da senha e o botão
+              onTouchStart={() => handleInputClick('senha')}
+              secureTextEntry
+            />
+            <TouchableOpacity
+              style={styles.editButton}
+              onPress={toggleEditSenha}
+            >
+              <Icon as={MaterialIcons} name={isEditingSenha ? "check" : "edit"} size={6} color="#FFFFFF" />
+            </TouchableOpacity>
+          </View>
+
+          {/* Container para o botão de excluir conta */}
+          <View style={styles.deleteButtonContainer}>
+            <Button
+              style={styles.deleteButton}
+              onPress={handleDeleteAccount}
+            >
+              <Text style={styles.deleteButtonText}>Excluir Conta</Text>
+            </Button>
+          </View>
+        </VStack>
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingTop: 40, 
-    paddingBottom: 10,
-    paddingHorizontal: 10, 
-    backgroundColor: '#fff', 
-    borderBottomWidth: 2,
-    borderBottomColor: '#FC9904', 
+  container: {
+    flex: 1,
   },
-  backButton: {
-    marginRight: 10,
-  },
-  headerTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center', 
-    flex: 1, // Permite que o título ocupe o espaço restante
-  },
-  content: {
+  imageBackground: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    fontSize: 24,
+  vStack: {
+    justifyContent: 'center',
+    paddingBottom: 250,
+  },
+  textContainer: {
+    width: '100%',
+    alignItems: 'flex-start',
+    marginBottom: 3,
+  },
+  inputContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  editButton: {
+    marginLeft: 10,
+  },
+  backButton: {
+    position: 'absolute',
+    top: 45,
+    left: 20,
+  },
+  deleteButtonContainer: {
+    width: '100%',
+    alignItems: 'center', // Centraliza o botão horizontalmente
+    marginTop: 40, // Espaço antes do botão
+  },
+  deleteButton: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 25,
+    borderColor: '#FC9904',
+    borderWidth: 1,
+    width: '80%', // Largura do botão
+    paddingVertical: 12,
+  },
+  deleteButtonText: {
+    color: '#FC9904',
+    fontSize: 16,
+    textAlign: 'center',
   },
 });
--- app/(drawer)/time.js
+++ app/(drawer)/time.js
@@ -1,57 +1,209 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons'; // Biblioteca de ícones
+import React, { useState, useContext } from 'react';
+import {
+  VStack,
+  Heading,
+  Input,
+  Text,
+  FlatList,
+  Box,
+  Icon,
+  useToast,
+} from 'native-base';
+import { TouchableOpacity, ImageBackground, StyleSheet, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'; 
+import { useRouter } from 'expo-router';
+//import GlobalContext from '../globalcontext.js';
 
-export default function Conta() {
-  const navigation = useNavigation();
+export default function EditTeamScreen() {
+  const router = useRouter(); 
+  const toast = useToast();
+
+  const [localTeamName, setLocalTeamName] = useState('INVENCÍVEIS');
+  const [localSelectedEmblem, setLocalSelectedEmblem] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const emblemData = [
+    { id: '1', name: 'Emblema 1' },
+    { id: '2', name: 'Emblema 2' },
+    { id: '3', name: 'Emblema 3' },
+    { id: '4', name: 'Emblema 4' },
+    { id: '5', name: 'Emblema 5' },
+    { id: '6', name: 'Emblema 6' },
+    { id: '7', name: 'Emblema 7' },
+    { id: '8', name: 'Emblema 8' },
+    { id: '9', name: 'Emblema 9' },
+    { id: '10', name: 'Emblema 10' },
+  ];
+
+  const handleEmblemSelect = (id) => {
+    setLocalSelectedEmblem(id);
+    toast.show({
+      title: "Sucesso",
+      description: "Emblema alterado com sucesso!",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+  };
+
+  const toggleEditMode = () => {
+    setIsEditing(!isEditing);
+    if (isEditing) {
+      toast.show({
+        title: "Sucesso",
+        description: "Nome editado com sucesso!",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
+
+  const handleInputClick = () => {
+    if (!isEditing) {
+      toast.show({
+        title: "Atenção",
+        description: "Clique no ícone de lápis para editar o nome.",
+        status: "warning",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
 
   return (
-    <View style={{ flex: 1 }}>
-      {/* Header */}
-      <View style={styles.header}>
-        <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
-          <Ionicons name="arrow-back" size={24} color="black" />
-        </TouchableOpacity>
-        <Text style={styles.headerTitle}>Meu Time</Text>
-      </View>
-      <View style={styles.headerUnderline} />
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('../../assets/images/layoutDrawerTime.jpg')} 
+        style={styles.imageBackground}
+        resizeMode="cover"
+      >
+        <VStack
+          width="85%"
+          space={5}
+          alignItems="center"
+          padding={4}
+          borderRadius="lg"
+          style={styles.vStack}
+        >
+          <TouchableOpacity
+            style={styles.backButton}
+            onPress={() => router.push('/(drawer)/(tabs)/home')}
+          >
+            <Icon as={MaterialIcons} name="arrow-back" size={10} color="#FFFFFF" />
+          </TouchableOpacity>
 
-      {/* Main Content */}
-      <View style={styles.content}>
-        <Text style={styles.text}>Conteúdo do Meu Time</Text>
-        {/* Adicione outros elementos ou botões conforme necessário */}
-      </View>
+          <Heading size="lg" color="#FFFFFF" mb={4}>
+            SUA EQUIPE DE BASQUETE!
+          </Heading>
+
+          <View style={styles.textContainer}>
+            <Text fontSize={25} color="#FFFFFF" textAlign="left">
+              Nome:
+            </Text>
+          </View>
+
+          <View style={styles.inputContainer}>
+            <Input
+              placeholder="Nome do time"
+              variant="filled"
+              width="100%"
+              backgroundColor="#D9D9D9"
+              borderRadius="full"
+              height={50}
+              fontSize={16}
+              value={localTeamName}
+              onChangeText={setLocalTeamName}
+              isReadOnly={!isEditing}
+              mb={4}
+              onTouchStart={handleInputClick} 
+            />
+            <TouchableOpacity
+              style={styles.editButton}
+              onPress={toggleEditMode}
+            >
+              <Icon as={MaterialIcons} name={isEditing ? "check" : "edit"} size={6} color="#FFFFFF" />
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.textContainer}>
+            <Text fontSize={25} color="#FFFFFF" textAlign="left">
+              Emblema:
+            </Text>
+          </View>
+
+          <View style={styles.separator} />
+
+          <FlatList
+            horizontal
+            data={emblemData}
+            renderItem={({ item }) => (
+              <TouchableOpacity onPress={() => handleEmblemSelect(item.id)}>
+                <Box
+                  padding={5}
+                  borderColor={
+                    localSelectedEmblem === item.id ? '#FC9904' : '#D9D9D9'
+                  }
+                  borderWidth={1}
+                  borderRadius="full"
+                  m={2}
+                  backgroundColor={
+                    localSelectedEmblem === item.id ? '#FFFFFF' : 'transparent'
+                  }
+                >
+                  <Text
+                    color={localSelectedEmblem === item.id ? '#FC9904' : '#FFFFFF'}
+                  >
+                    {item.name}
+                  </Text>
+                </Box>
+              </TouchableOpacity>
+            )}
+            keyExtractor={(item) => item.id}
+            showsHorizontalScrollIndicator={false}
+          />
+        </VStack>
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingTop: 40, // Espaçamento superior para a área segura
-    paddingBottom: 10,
-    paddingHorizontal: 10, // Adicionado para espaçamento horizontal
-    backgroundColor: '#fff', // Cor de fundo do header
-    borderBottomWidth: 2,
-    borderBottomColor: '#FC9904', // Cor laranja do traço
+  container: {
+    flex: 1,
   },
-  backButton: {
-    marginRight: 10,
-  },
-  headerTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center', // Centraliza o título
-    flex: 1, // Permite que o título ocupe o espaço restante
-  },
-  content: {
+  imageBackground: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    fontSize: 24,
+  vStack: {
+    justifyContent: 'center',
+    paddingBottom: 250,
+  },
+  textContainer: {
+    width: '100%',
+    alignItems: 'flex-start',
+    marginBottom: 3,
+  },
+  inputContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  editButton: {
+    marginLeft: 10,
+  },
+  backButton: {
+    position: 'absolute',
+    top: 45,
+    left: 20,
+  },
+  separator: {
+    width: '100%',
+    height: 1,
+    backgroundColor: '#D9D9D9',
+    marginVertical: 0,
   },
 });
